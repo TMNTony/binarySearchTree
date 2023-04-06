dec03511cd1755be6001c3e5cbf0ac0e17ba077d
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-04-06 15:44:47 -0500

    Add buildTree function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import "./style.css";
 import Node from "./node.js";
 
+
+
 class Tree {
   constructor(array) {
     this.root = this.buildTree(array);
+  }
+
+  function buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) return null;
+  
+    const mid = parseInt((start + end) / 2);
+    const root = Node(array[mid]);
+  
+    root.left = buildTree(array, start, mid - 1);
+    root.right = buildTree(array, mid + 1, end);
+  
+    return root;
   }
 }
