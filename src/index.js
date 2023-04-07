b5a4f51c47d6f28707ca2d574b7aecbc081f42e1
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-04-06 21:28:22 -0500

    Add itterative insert method

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 import "./style.css";
 import Node from "./node.js";
 
-
-
 class Tree {
   constructor(array) {
-    this.root = this.buildTree(array);
+    this.root = null;
   }
 
-  function buildTree(array, start = 0, end = array.length - 1) {
-    if (start > end) return null;
-  
-    const mid = parseInt((start + end) / 2);
-    const root = Node(array[mid]);
-  
-    root.left = buildTree(array, start, mid - 1);
-    root.right = buildTree(array, mid + 1, end);
-  
-    return root;
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let tree = this.root;
+
+    while (true) {
+      if (value < tree.value) {
+        if (!tree.left) {
+          tree.left = newNode;
+          return this;
+        }
+        tree = tree.left;
+      } else {
+        if (!tree.right) {
+          tree.right = newNode;
+          return this;
+        }
+        tree = tree.right;
+      }
+    }
+    return this;
+  }
+
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
   }
 }
+
+const tree = new Tree();
+
+tree.insert(1);
+tree.insert(6);
